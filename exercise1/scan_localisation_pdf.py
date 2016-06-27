85f07195c4827e1d8feae593c686f2789b49b5c8
mroussel1 <marion.roussel1@gmail.com>
2016-06-27 10:06:49 +0530

    exercice1 running with all locations
    All locations
    Comments
    Normalisation in uppercases to better comparison

--- exercise1/scan_localisation_pdf.py
+++ exercise1/scan_localisation_pdf.py
@@ -16,10 +16,12 @@ def main():
 	
 	start = time.time()
 	
+	##### To load the possible locations that have to be found in the document #####
 	f = open("locations.txt")
 	locations = f.read().splitlines()
 	f.close
     
+	##### To define the input and output files #####
 	pdf_name = INPUT
 	output_name = OUTPUT
     
@@ -30,33 +32,39 @@ def main():
 		if sys.argv[2]:
 			output_name = sys.argv[2]
     
+	##### To find the locations and their pages #####
 	res = pdfparser(pdf_name, locations)
     
+	###### To write the locations and the pages where they were found on a file as well as directly on the terminal ######
 	print "------------------------------------------------------------"
 	print "Results (you can find them in %s also) :"%output_name
-    
-	f = open(output_name, "w")
-    
+     
+	info_list = []
 	for item in res:
 		if item["pages"]:
 			info = "%s -> Found on page %s"%(item["name"], ", ".join(str(page) for page in item["pages"]))
 			print info
-			f.write(info + "\n")
-                
-	f.close()
-
+			info_list.append(info)
+     
+	info_list.sort() # to sort the locations
+	
+	f = open(output_name, "w")
+	for item in info_list:
+		f.write(item +"\n")
+	f.close()	
+           
 	end = time.time()
 	elapsed = end - start
 	print "Time taken: ", elapsed, "seconds."
 
-def pdfparser(data, locations):
+def pdfparser(data, locations): # to find the locations and their pages
 	res = [0] * len(locations)
 	fp = file(data, 'rb')
 	rsrcmgr = PDFResourceManager()
 	codec = 'utf-8'
 	laparams = LAParams()
 	page_list = PDFPage.get_pages(fp)
-    
+
 	item_list = []
 	for place in locations:
 		new_item = dict()
@@ -73,17 +81,18 @@ def pdfparser(data, locations):
         
 		page_num = index + 1
 		interpreter.process_page(page)
-		data = retstr.getvalue()
+		info = retstr.getvalue()
 		for item in item_list:
-			if re.search("\W%s\W"%item["name"], data):
-				print "Found %s on page %d"%(item["name"], page_num)
+			if re.search("\W%s\W"%item["name"].upper(), info.upper()): # important to compare all the strings in uppercases (or in lowercases) to find all locations 
+				#print "Found %s on page %d"%(item["name"], page_num) #print the founded locations in the terminal, useful during the implementing stage 
 				item["pages"].append(page_num)
 				item["found"] = item["found"] + 1
         
-        #f = open("pages/page%d.txt"%page_num, "w")
-        #locations = f.write(data)
-        #f.close()
+		#f = open("pages/page%d.txt"%page_num, "w") #to check the page information in a txt format, useful during the implementing stage
+		#f.write(info)
+		#f.close()
 
+	fp.close() 
 	return item_list
                 
 if __name__ == '__main__':
